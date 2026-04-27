b0013280c7fc1e680c3bd4ab117c88b484aaac39
Edvinas Bureika <edvinasbureika@gmail.com>
2026-04-27 20:03:51 +0300

    comments

--- src/dataImporter/dataImporter.h
+++ src/dataImporter/dataImporter.h
@@ -4,9 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/// @brief Opens file for reading data
+/// @param filename file name
+/// @return FILE instance
 FILE* data_importer_open_file(const char* filename);
+
+/// @brief Gets file size
+/// @param f FILE instance
+/// @return file size
 long data_importer_get_file_size(FILE* f);
+
+/// @brief Returns char* data buffer read from file
+/// @param f File instance
+/// @return data buffer
 char* data_importer_get_buffer(FILE* f);
+
+/// @brief Cleans up allocated memory
+/// @param f FILE instance
+/// @param buffer data buffer
 void data_importer_cleanup(FILE* f, char* buffer);
 
 #endif
--- src/locationService/locationService.h
+++ src/locationService/locationService.h
@@ -9,8 +9,19 @@
 #include "../http/httpClient.h"
 #include "../lib/cjson/cJSON.h"
 
+/// @brief Gets user location and stores it in json format in root
+/// @param root address to root ptr that stores json data
+/// @return status code
 int getLocation(cJSON** root);
+
+/// @brief Gets user's city string from JSON root
+/// @param root JSON root with data
+/// @return City name string
 char* getCity(cJSON* root);
+
+/// @brief Gets user's country string from JSON root
+/// @param root JSON root with data
+/// @return Country name string
 char* getCountry(cJSON* root);
 
 #endif
